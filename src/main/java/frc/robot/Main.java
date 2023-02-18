ef227346888e0d532e173485f82e4ac81486352b
Alex Naehu <naehuthesnail@gmail.com>
2023-02-17 18:03:27 -0800

    quick change to main.java

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -2,9 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Robot;
+package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
+import frc.robot.Robot.Robot;
 
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
